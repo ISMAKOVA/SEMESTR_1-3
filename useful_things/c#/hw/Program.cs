af052dc828a7a415cc24abcb5cd11355bcb45231
ISMAKOVA <48486359+ISMAKOVA@users.noreply.github.com>
2019-04-07 13:44:59 +0500

    done

--- useful_things/c#/hw/Program.cs
+++ useful_things/c#/hw/Program.cs
@@ -6,8 +6,11 @@ namespace hw
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("число");
+            int c = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("введите степень");
+            Console.WriteLine("#1");
             int n = Int32.Parse(Console.ReadLine());
-            int c = 2;
             int result = 1;
             int counter = 0;
             while (counter != n)
@@ -16,20 +19,29 @@ namespace hw
                 counter++;
             }
             Console.WriteLine(result);
+            Console.WriteLine("#2");
+            //int r = Int32.Parse(Console.ReadLine());
+            int rr = task2(n, c);
+            Console.WriteLine(rr);
         }
          static int task2(int n, int c)
         {
-            int res = 1;
-            int counter = 0;
-            while (n!=counter)
+            int result = 1;
+           //int counter = 0;
+            while (n>0)
             {
-                if (n % 1==0)
-                    res *= c;
-                c *= c;
-                n++;
-                counter++;
+                if (n % 2 == 0)
+                {
+                    n =n/ 2;
+                    c *= c;
+                }
+                else
+                {
+                    n--;
+                    result *= c;
+                }
             }
-            return res;
+            return result;
         }
     }
 }
