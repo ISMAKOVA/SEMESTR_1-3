b6d9e71dac8d69a5a1c4547ef73ed07c432917a5
ISMAKOVA <48486359+ISMAKOVA@users.noreply.github.com>
2020-10-23 19:33:18 +0500

    Add files via upload

---
+++ Random_projects/rpp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace rpp
+{
+    class MainClass
+    {
+
+        public static void Main(string[] args)
+        {
+            i = 1;
+
+
+        }
+        static int i = 0;
+
+
+
+    }
+
+}
---
+++ Random_projects/work/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace work
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
